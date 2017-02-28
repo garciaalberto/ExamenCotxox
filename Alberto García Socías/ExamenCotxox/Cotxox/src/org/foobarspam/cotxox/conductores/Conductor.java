f1bab7fa0419482623e453e5cdab2e9d11121e17
Alberto García <Bertooooo10@gmail.com>
2017-02-28 17:19:01 +0100

    Update Conductor.java

--- Alberto García Socías/ExamenCotxox/Cotxox/src/org/foobarspam/cotxox/conductores/Conductor.java
+++ Alberto García Socías/ExamenCotxox/Cotxox/src/org/foobarspam/cotxox/conductores/Conductor.java
@@ -9,6 +9,7 @@ public class Conductor {
 	private String modelo = null;
 	private ArrayList<Byte> valoraciones = new ArrayList<>();
 	private boolean ocupado = false;
+	private Double valoracionMedia = 0d;
 	
 	// Constructores
 	public Conductor() {
@@ -41,11 +42,7 @@ public class Conductor {
 	}
 
 	public double getValoracion() {
-		double valoracion = 0d;
-		for (Byte valor : getValoraciones()){
-			valoracion += valor;
-		}
-		return valoracion/ getValoraciones().size();
+		return getValoracionMedia();
 	}
 
 	public ArrayList<Byte> getValoraciones() {
@@ -53,7 +50,8 @@ public class Conductor {
 	}
 
 	public void setValoracion(Byte valoracion) {
-		this.valoraciones.add(valoracion);
+			valoraciones.add(valoracion);
+			this.actualizarMedia();
 	}
 
 	public boolean isOcupado() {
@@ -62,6 +60,26 @@ public class Conductor {
 
 	public void setOcupado(boolean ocupado) {
 		this.ocupado = ocupado;
+	}
+
+	public void setValoracionMedia(Double valoracionMedia) {
+		this.valoracionMedia = valoracionMedia;
+	}
+
+	public Double getValoracionMedia() {
+		return valoracionMedia;
+	}
+	
+	public void actualizarMedia() {
+
+		int suma = 0;
+
+		for (Byte valoracion : this.getValoraciones()) {
+			suma = suma + valoracion;
+		}
+		int longitud = this.getValoraciones().size();
+		double media = suma / longitud;
+		this.setValoracionMedia(media);
 	}
 	
 	
